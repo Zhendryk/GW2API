f1dddef42b5bcfb617a1eeae3cf96cf52dbed9c3
Zhendryk <jonathan.bailey@comcast.net>
2018-10-18 15:25:51 -0400

    Fixed Guild Log JSON model.

--- GW2API/JSON Models/Guild/GuildLog.swift
+++ GW2API/JSON Models/Guild/GuildLog.swift
@@ -12,45 +12,23 @@ public struct GuildLog: Decodable {
     public let user: String
     public let type: String
     
-    public let joined: String?
-    public let invited: invited?
-    public let kick: kick?
-    public let rank_change: rankChange?
-    public let treasury: treasury?
-    public let stash: stash?
-    public let motd: String?
-    public let upgrade: upgrade?
-}
-
-public struct invited: Decodable {
-    public let invited_by: String
-}
-
-public struct kick: Decodable {
-    public let kicked_by: String
-}
-
-public struct rankChange: Decodable {
-    public let changed_by: String
-    public let old_rank: String
-    public let new_rank: String
-}
-
-public struct treasury: Decodable {
-    public let item_id: Int
-    public let count: Int
-}
-
-public struct stash: Decodable {
-    public let operation: String
-    public let item_id: Int
-    public let count: Int
-    public let coins: Int
-}
-
-public struct upgrade: Decodable {
-    public let action: String
-    public let upgrade_id: Int
+    public let invited_by: String?
+    
+    public let kicked_by: String?
+    
+    public let changed_by: String?
+    public let old_rank: String?
+    public let new_rank: String?
+    
+    public let item_id: Int?
     public let count: Int?
+    
+    public let operation: String?
+    public let coins: String?
+    
+    public let action: String?
+    public let upgrade_id: String?
     public let recipe_id: Int?
+
+    public let motd: String?
 }
